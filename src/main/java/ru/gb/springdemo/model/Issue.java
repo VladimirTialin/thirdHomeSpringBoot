fb9fd5f10ec1b67e5ece829aae1308eb15213ac9
Vladimir <wel.tall@yandex.ru>
2024-01-26 20:18:06 +0300

    homework6_work

--- src/main/java/ru/gb/springdemo/model/Issue.java
+++ src/main/java/ru/gb/springdemo/model/Issue.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Table(name="issues")
 public class Issue {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   @Column(name = "bookId")
   private long bookId;
--- src/main/java/ru/gb/springdemo/model/Reader.java
+++ src/main/java/ru/gb/springdemo/model/Reader.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class Reader {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
     @Column(name="readerName")
   private String name;
